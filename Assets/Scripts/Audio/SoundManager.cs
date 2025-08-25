8ad065716c1828228283a4a5e022eaf160240239
splicioKhoa <133840949+codebender04@users.noreply.github.com>
2025-08-25 23:10:36 +0700

    Add Sound Manager And Play Walking Sounds

---
+++ Assets/Scripts/Audio/SoundManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : Singleton<SoundManager>
+{
+
+
+    private const string PLAYER_PREFS_SOUND_EFFECTS_VOLUME = "SoundEffectsVolume";
+
+    [SerializeField] private AudioClipRefsSO audioClipRefsSO;
+
+
+    private float volume = 1f;
+
+
+    private void Awake()
+    {
+        volume = PlayerPrefs.GetFloat(PLAYER_PREFS_SOUND_EFFECTS_VOLUME, 1f);
+    }
+    private void PlaySound(AudioClip[] audioClipArray, Vector3 position, float volume = 1f)
+    {
+        PlaySound(audioClipArray[Random.Range(0, audioClipArray.Length)], position, volume);
+    }
+
+    private void PlaySound(AudioClip audioClip, Vector3 position, float volumeMultiplier = 1f)
+    {
+        AudioSource.PlayClipAtPoint(audioClip, position, volumeMultiplier * volume);
+    }
+    public void PlayFootstepWalkSound(Vector3 position, float volume)
+    {
+        PlaySound(audioClipRefsSO.footstepWalk, position, volume);
+    }
+    public void PlayFootstepCrouchSound(Vector3 position, float volume)
+    {
+        PlaySound(audioClipRefsSO.footstepCrouch, position, volume);
+    }
+    public void ChangeVolume()
+    {
+        volume += .1f;
+        if (volume > 1f)
+        {
+            volume = 0f;
+        }
+
+        PlayerPrefs.SetFloat(PLAYER_PREFS_SOUND_EFFECTS_VOLUME, volume);
+        PlayerPrefs.Save();
+    }
+
+    public float GetVolume()
+    {
+        return volume;
+    }
+
+
+}
