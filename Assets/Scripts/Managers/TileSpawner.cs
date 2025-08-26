38d2d202fecbf04570ea06272d62b0e284ae52d3
F1nndegamer <140960106+F1nndegamer@users.noreply.github.com>
2025-08-26 17:34:37 +0200

    Fix+ BEDROOM UPDATE

--- Assets/Scripts/Managers/TileSpawner.cs
+++ Assets/Scripts/Managers/TileSpawner.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using static UnityEditor.PlayerSettings;
 
 public class TileSpawner : MonoBehaviour
 {
@@ -10,6 +9,14 @@ public class TileSpawner : MonoBehaviour
     {
         RoomHold hold = GetComponent<RoomHold>();
         Vector2 roomSize = hold.size;
-        Instantiate(tilePrefab, transform.position, Quaternion.identity).GetComponent<SpriteRenderer>().size = roomSize;
+        GameObject tile = Instantiate(tilePrefab, transform.position, Quaternion.identity, transform);
+        SpriteRenderer sr = tile.GetComponent<SpriteRenderer>();
+        sr.size = roomSize;
+        Vector3 parentScale = transform.lossyScale;
+        tile.transform.localScale = new Vector3(
+            1f / parentScale.x,
+            1f / parentScale.y,
+            1f / parentScale.z
+        );
     }
 }
