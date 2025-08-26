6810425450fad667bd21aee1ec861905867596c3
splicioKhoa <133840949+codebender04@users.noreply.github.com>
2025-08-26 21:16:04 +0700

    Add Tiled Floor Tiles

--- Assets/Scripts/Managers/TileSpawner.cs
+++ Assets/Scripts/Managers/TileSpawner.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 public class TileSpawner : MonoBehaviour
 {
@@ -9,17 +10,6 @@ public class TileSpawner : MonoBehaviour
     {
         RoomHold hold = GetComponent<RoomHold>();
         Vector2 roomSize = hold.size;
-
-        int tilesX = Mathf.RoundToInt(roomSize.x / tileSize);
-        int tilesY = Mathf.RoundToInt(roomSize.y / tileSize);
-
-        for (int x = 0; x < tilesX; x++)
-        {
-            for (int y = 0; y < tilesY; y++)
-            {
-                Vector3 pos = new Vector3(x * tileSize, y * tileSize, 0);
-                Instantiate(tilePrefab, pos + transform.position, Quaternion.identity, transform);
-            }
-        }
+        Instantiate(tilePrefab, transform.position, Quaternion.identity).GetComponent<SpriteRenderer>().size = roomSize;
     }
 }
